9e54cee981cea0002c41eb9076fe85fa3491aeaa
Hajimojikun <ichig9.xx@gmail.com>
2021-05-28 12:55:25 +0900

    ユーザー管理機能　手直し１回目

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,16 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
          with_options presence: true do
-         validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
-         validates :nickname
-         validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/}
-         validates :last_name,  format: { with: /\A[ぁ-んァ-ン一-龥]/}
-         validates :first_name_kana, format:{with: /\A[ァ-ヶー－]+\z/}
-         validates :last_name_kana,  format:{with: /\A[ァ-ヶー－]+\z/}
-         validates :birth_day
+            validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
+            validates :nickname
+            validates :birth_day
+              with_options format: { with: /\A[ぁ-んァ-ン一-龥]/} do
+                validates :first_name
+                validates :last_name
+                  with_options format:{with: /\A[ァ-ヶー－]+\z/} do
+                    validates :first_name_kana
+                    validates :last_name_kana
+                  end
+              end
          end
 end
