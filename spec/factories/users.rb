300cd275636d1397010d41d0353e758a27fe47d8
Hajimojikun <ichig9.xx@gmail.com>
2021-05-28 12:05:36 +0900

    userモデル　テストコード

---
+++ spec/factories/users.rb
@@ -0,0 +1,15 @@
+FactoryBot.define do 
+  factory :user do
+    name = Gimei.name
+    email {Faker::Internet.free_email}
+    password = Faker::Internet.password(min_length:6)
+    password {password}
+    password_confirmation {password}
+    nickname {Faker::Name.name}
+    last_name {name.last.kanji}
+    first_name {name.first.kanji}
+    last_name_kana {name.last.katakana}
+    first_name_kana {name.first.katakana}
+    birth_day {Faker::Date.birthday(min_age:18, max_age:60)}
+  end
+end
