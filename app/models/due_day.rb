7f3e5130b48c9f6d68d3056008a96f92b42da592
Hajimojikun <ichig9.xx@gmail.com>
2021-05-29 21:02:38 +0900

    ActiveHash　作成

---
+++ app/models/due_day.rb
@@ -0,0 +1,10 @@
+class DueDay < ActiveHash::Base
+  self.date = [
+    {id: 1, name: "---"},
+    {id: 2, name: "1日〜２日で発送"},
+    {id: 3, name: "２日〜３日で発送"},
+    {id: 4, name: "４日〜７日で発送"}
+  ]
+  include ActiveHash::Associations
+  has_many :items
+end
