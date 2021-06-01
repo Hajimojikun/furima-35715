04b35dc88fbf5810fb83ccc6831317a6a82b77fc
Hajimojikun <ichig9.xx@gmail.com>
2021-06-01 20:09:00 +0900

    商品情報編集機能　手直し１回目

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,9 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only:[:new,:create,:edit,:update]
+  before_action :make_instance, only:[:show, :edit, :update]
+  before_action :move_to, only:[:update,:dastroy]
+  
+
 
   def index
     @items = Item.all.order("created_at DESC")
@@ -19,16 +23,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
-    redirect_to root_path unless @item.user.id == current_user.id
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(items_params)
       redirect_to item_path(@item.id)
     else
@@ -38,7 +38,6 @@ class ItemsController < ApplicationController
 
   def destroy
     item = Item.find(params[:id])
-    redirect_to root_path unless item.user.id == current_user.id
     if item.destroy
       redirect_to root_path
     end
@@ -49,5 +48,13 @@ class ItemsController < ApplicationController
     params.require(:item).permit(
       :name, :description, :price, :image, :category_id, :state_id, :who_pay_id, :due_day_id, :prefecture_id
     ).merge(user_id: current_user.id)
+  end
+  
+  def move_to
+    redirect_to root_path unless @item.user.id == current_user.id
+  end
+
+  def make_instance
+    @item = Item.find(params[:id])
   end
 end
