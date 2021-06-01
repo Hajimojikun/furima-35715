5acadf3b319bf20c9b58a8af688948fea5518de2
Hajimojikun <ichig9.xx@gmail.com>
2021-06-01 20:17:54 +0900

    商品情報編集機能　手直し２回目

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only:[:new,:create,:edit,:update]
   before_action :make_instance, only:[:show, :edit, :update]
-  before_action :move_to, only:[:update,:dastroy]
+  before_action :move_to, only:[:edit, :update,:destroy]
   
 
 
