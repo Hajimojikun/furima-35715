46976464285fbd937fed77365844aa81226df19b
Hajimojikun <ichig9.xx@gmail.com>
2021-06-04 17:33:05 +0900

    商品購入機能　手直し３回目

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only:[:new,:create,:edit,:update]
   before_action :make_instance, only:[:show, :edit, :update]
-  before_action :search_bought_item, only:[:show, :edit]
+  before_action :search_bought_item, only:[:show, :edit, :update, :destroy]
   before_action :move_to, only:[:edit, :update,:destroy]
   
 
