7cffabd4fa9d0e5c03b74b4a5148bfc1ff9ed947
Hajimojikun <ichig9.xx@gmail.com>
2021-06-04 15:19:16 +0900

    モデルテストコード

--- spec/factories/buy_item_where_tos.rb
+++ spec/factories/buy_item_where_tos.rb
@@ -2,18 +2,12 @@ FactoryBot.define do
   factory :buy_item_where_to do
     address = Gimei.address
 
-    number                {4242424242424242}
-    exp_month             {3}
-    exp_year              {23}
-    cvc                   {123}
-    post_code             {123-1234}
+    token                 {"tok_abcdefghijk00000000000000000"}
+    post_code             {"123-1234"}
     city                  {address.city.kanji}
     street                {address.town.kanji}
     name_of_the_building  {address.kanji}
     phone_number          {Faker::Number.number(digits: 11)}
     prefecture_id         {2}
-
-    association :user
-    association :item
   end
 end
